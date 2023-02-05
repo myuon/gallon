8c8791f52dabae9a3f0ca0e4e7772846a6a42a37
myuon <ioi.joi.koi.loi@gmail.com>
2023-02-05 18:59:50 +0900

    fix: command use

--- cmd/run.go
+++ cmd/run.go
@@ -12,7 +12,7 @@ import (
 )
 
 var RunCmd = &cobra.Command{
-	Use:   "RunGallon",
+	Use:   "run",
 	Short: "Run a migration",
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
